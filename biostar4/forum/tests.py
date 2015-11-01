a919937d73e82c3e8e0eeed51217a19bef222999
Istvan Albert <istvan.albert@gmail.com>
2015-11-01 10:21:42 -0500

    adding tests

--- biostar4/forum/tests.py
+++ biostar4/forum/tests.py
@@ -5,25 +5,51 @@ from biostar4.forum.models import User, Profile, Post
 
 
 class SiteTest(TestCase):
+
     def setUp(self):
         self.c = Client()
+        self.EQ = self.assertEqual
+        self.TRUE = self.assertTrue
+        self.IN = self.assertContains
 
-    def test_main_site(self):
-        '''
-        Tests that the site can be instantiated
-        '''
-        EQ = self.assertEqual
+    def test_links(self):
+        " Test links"
 
-        # Main page works.
-        r = self.c.get('/')
-        EQ(r.status_code, 200)
+        links = "home signup login logout planet user_list".split()
+        for link in links:
+            url = reverse(link)
+            r = self.c.get(url)
+            self.EQ(r.status_code, 200)
 
-        # This should redirect to login.
-        me = reverse("me")
-        r = self.c.get(me)
-        EQ(r.status_code, 302)
+    def test_redirects(self):
+        links = "me user_edit post_new messages votes my_site".split()
+        for link in links:
+            url = reverse(link)
+            r = self.c.get(url)
+            self.EQ(r.status_code, 302)
 
-        # Sign up a user.
+class UserTest(SiteTest):
+
+
+    def test_login(self):
+
         signup = reverse("signup")
-        r = self.c.get(signup)
-        EQ(r.status_code, 200)
+        user = User(email="foo@foo.com", password="$%!3456ABC")
+
+        r = self.c.post(signup, {'email': user.email, 'password': "123"})
+        # Should fail and stay on the same page.
+        self.EQ(r.status_code, 200)
+        self.IN(r, "error")
+
+        r = self.c.post(signup, {'email': user.email, 'password': user.password})
+        # Should succeed
+        self.EQ(r.status_code, 302)
+
+        # Logged in users can access their pages.
+        links = "post_new messages votes my_site".split()
+        for link in links:
+            url = reverse(link)
+            r = self.c.get(url)
+            self.EQ(r.status_code, 200)
+
+
